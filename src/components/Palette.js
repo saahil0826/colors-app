7ba12606302c16fcea59c7dbd02217eab97a8670
saahil0826 <snawalkha0826@gmail.com>
2020-10-13 21:08:05 +0530

    create-react-app and palette.js

---
+++ src/components/Palette.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+// import ColorBox from "./ColorBox.js";
+
+
+class Palette extends Component {
+  render() {
+    // const colorBoxes = this.props.palette.colors.map(c => (
+    //   <ColorBox background={c.color} name={c.name} />
+    // ));
+    return (
+      <div className="Palette">
+      <div className='Palette-colors'>palette</div>
+      </div>
+    );
+  }
+}
+
+export default Palette;
