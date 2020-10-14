550b987fad8516b54d93b6aa55a93d2b89199285
saahil0826 <snawalkha0826@gmail.com>
2020-10-14 14:31:43 +0530

    Added palette footer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { generatePalette } from "./colorHelpers";
 function App() {
   return (
     <div className="App">
-    <Palette palette={generatePalette(seedColors[4])} />
+    <Palette palette={generatePalette(seedColors[2])} />
     </div>
   );
 }
