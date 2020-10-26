1e062751e2dddad8fdfbcbd9fc0c079a4c6c214b
saahil0826 <snawalkha0826@gmail.com>
2020-10-26 19:58:14 +0530

    Add SavePalette

--- src/App.js
+++ src/App.js
@@ -2,27 +2,41 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Palette from "./components/Palette.js";
 import PaletteList from "./components/PaletteList";
-import SingleColorPalette from './components/SingleColorPalette.js'
+import SingleColorPalette from "./components/SingleColorPalette.js";
 import NewPaletteForm from "./components/NewPaletteForm";
-
 import seedColors from "./seedColors.js";
 import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { palettes: seedColors };
+    this.savePalette = this.savePalette.bind(this);
+    this.findPalette = this.findPalette.bind(this);
+  }
   findPalette(id) {
-    return seedColors.find(function(palette) {
+    return this.state.palettes.find(function(palette) {
       return palette.id === id;
     });
+  }
+  savePalette(newPalette) {
+    this.setState({ palettes: [...this.state.palettes, newPalette] });
   }
   render() {
     return (
       <Switch>
-      <Route exact path='/palette/new' render={() => <NewPaletteForm />} />
+        <Route
+          exact
+          path="/palette/new"
+          render={routeProps => (
+            <NewPaletteForm savePalette={this.savePalette} {...routeProps} />
+          )}
+        />
         <Route
           exact
           path="/"
           render={routeProps => (
-            <PaletteList palettes={seedColors} {...routeProps} />
+            <PaletteList palettes={this.state.palettes} {...routeProps} />
           )}
         />
         <Route
@@ -37,17 +51,17 @@ class App extends Component {
           )}
         />
         <Route
-       exact
-       path='/palette/:paletteId/:colorId'
-       render={routeProps => (
-         <SingleColorPalette
-           colorId={routeProps.match.params.colorId}
-           palette={generatePalette(
-             this.findPalette(routeProps.match.params.paletteId)
-           )}
-         />
-       )}
-     />
+          exact
+          path="/palette/:paletteId/:colorId"
+          render={routeProps => (
+            <SingleColorPalette
+              colorId={routeProps.match.params.colorId}
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.paletteId)
+              )}
+            />
+          )}
+        />
       </Switch>
 
       // <div>
--- src/components/NewPaletteForm.js
+++ src/components/NewPaletteForm.js
@@ -82,11 +82,12 @@ class NewPaletteForm extends Component {
       open: true,
       currentColor: "teal",
       newName: "",
-     colors: [{ color: "blue", name: "blue" }]
+      colors: [{ color: "blue", name: "blue" }]
     };
     this.updateCurrentColor = this.updateCurrentColor.bind(this);
     this.addNewColor = this.addNewColor.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   componentDidMount() {
     ValidatorForm.addValidationRule("isColorNameUnique", value =>
@@ -113,13 +114,23 @@ class NewPaletteForm extends Component {
 
   addNewColor() {
     const newColor = {
-         color: this.state.currentColor,
-         name: this.state.newName
-       };
-       this.setState({ colors: [...this.state.colors, newColor], newName: "" });
-     }
-     handleChange(evt) {
-       this.setState({ newName: evt.target.value });
+      color: this.state.currentColor,
+      name: this.state.newName
+    };
+    this.setState({ colors: [...this.state.colors, newColor], newName: "" });
+  }
+  handleChange(evt) {
+    this.setState({ newName: evt.target.value });
+  }
+  handleSubmit() {
+    let newName = "New Test Palette";
+    const newPalette = {
+      paletteName: newName,
+      id: newName.toLowerCase().replace(/ /g, "-"),
+      colors: this.state.colors
+    };
+    this.props.savePalette(newPalette);
+    this.props.history.push("/");
   }
 
   render() {
@@ -130,29 +141,37 @@ class NewPaletteForm extends Component {
       <div className={classes.root}>
         <CssBaseline />
         <AppBar
-          position='fixed'
+          position="fixed"
+          color="default"
           className={classNames(classes.appBar, {
             [classes.appBarShift]: open
           })}
         >
           <Toolbar disableGutters={!open}>
             <IconButton
-              color='inherit'
-              aria-label='Open drawer'
+              color="inherit"
+              aria-label="Open drawer"
               onClick={this.handleDrawerOpen}
               className={classNames(classes.menuButton, open && classes.hide)}
             >
               <MenuIcon />
             </IconButton>
-            <Typography variant='h6' color='inherit' noWrap>
+            <Typography variant="h6" color="inherit" noWrap>
               Persistent drawer
             </Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.handleSubmit}
+            >
+              Save Palette
+            </Button>
           </Toolbar>
         </AppBar>
         <Drawer
           className={classes.drawer}
-          variant='persistent'
-          anchor='left'
+          variant="persistent"
+          anchor="left"
           open={open}
           classes={{
             paper: classes.drawerPaper
@@ -164,12 +183,12 @@ class NewPaletteForm extends Component {
             </IconButton>
           </div>
           <Divider />
-          <Typography variant='h4'>Design Your Palette</Typography>
+          <Typography variant="h4">Design Your Palette</Typography>
           <div>
-            <Button variant='contained' color='secondary'>
+            <Button variant="contained" color="secondary">
               Clear Palette
             </Button>
-            <Button variant='contained' color='primary'>
+            <Button variant="contained" color="primary">
               Random Color
             </Button>
           </div>
@@ -177,26 +196,26 @@ class NewPaletteForm extends Component {
             color={this.state.currentColor}
             onChangeComplete={this.updateCurrentColor}
           />
-          <ValidatorForm onSubmit={this.addNewColor} ref='form'>
-          <TextValidator
-            value={this.state.newName}
-            onChange={this.handleChange}
-            validators={["required", "isColorNameUnique", "isColorUnique"]}
-            errorMessages={[
-              "Enter a color name",
-              "Color name must be unique",
-              "Color already used!"
-            ]}
-          />
-          <Button
-            variant='contained'
-            type='submit'
-            color='primary'
-            style={{ backgroundColor: this.state.currentColor }}
-          >
-            Add Color
-          </Button>
-        </ValidatorForm>
+          <ValidatorForm onSubmit={this.addNewColor} ref="form">
+            <TextValidator
+              value={this.state.newName}
+              onChange={this.handleChange}
+              validators={["required", "isColorNameUnique", "isColorUnique"]}
+              errorMessages={[
+                "Enter a color name",
+                "Color name must be unique",
+                "Color already used!"
+              ]}
+            />
+            <Button
+              variant="contained"
+              type="submit"
+              color="primary"
+              style={{ backgroundColor: this.state.currentColor }}
+            >
+              Add Color
+            </Button>
+          </ValidatorForm>
         </Drawer>
         <main
           className={classNames(classes.content, {
